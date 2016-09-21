e5e2148c559a363a252958be02da2e1d9c216606
Xiaomeng Huang <huangxiaomeng@gmail.com>
2016-09-21 15:01:42 +0800

    SUMZ_XZ adjusted.

--- test.m
+++ test.m
@@ -4,7 +4,8 @@ load('test.mat');
 load('operator.mat');
 
 drhox=zeros(im,jm,kbm1+1);
-
+A=zeros(im,jm,kbm1+1);
+B=A;
 %drhox(:,:,1)= grav*(-zz(1)) * AXB_XY(dt) .* DXB_XY(rho(:,:,1));
 
 
@@ -17,7 +18,7 @@ for k=2:kbm1
 % % %                           *(rho(i,j,k)+rho(i,j,k-1)-rho(i-1,j,k)-rho(i-1,j,k-1))    ...
 % % %                          +grav*.25e0*(zz(k-1)+zz(k))*(dt(i,j)-dt(i-1,j))           ...
 % % %                           *(rho(i,j,k)+rho(i-1,j,k)-rho(i,j,k-1)-rho(i-1,j,k-1));
-            drhox(i,j,k)=grav*.25e0*(zz(k-1)-zz(k))*(dt(i,j)+dt(i-1,j))           ...
+            A(i,j,k)=grav*.25e0*(zz(k-1)-zz(k))*(dt(i,j)+dt(i-1,j))           ...
                           *(rho(i,j,k)+rho(i,j,k-1)-rho(i-1,j,k)-rho(i-1,j,k-1))    ...
                          +grav*.25e0*(zz(k-1)+zz(k))*(dt(i,j)-dt(i-1,j))           ...
                           *(rho(i,j,k)+rho(i-1,j,k)-rho(i,j,k-1)-rho(i-1,j,k-1));
@@ -26,12 +27,15 @@ for k=2:kbm1
 end
 
 
+
 rho_tmp=zeros(im,kb);
 for j=2:jmm1
     % get a 2D array from a 3D array. 
     rho_tmp(:,:)=rho(:,j,:); 
 % % %     drhox1(:,j,:) = SUMZ_XZ(-grav * DZB_XZ(repmat(zz,im,1)) .* AXB_XZ(repmat(dt(:,j),1,kb)) .* DXB_XZ( AZB_XZ(rho_tmp) ) ...
 % % %                      +grav * AZB_XZ(repmat(zz,im,1)) .* DXB_XZ(repmat(dt(:,j),1,kb)) .* DZB_XZ( AXB_XZ(rho_tmp) ));
-    drhox1(:,j,:) = (-grav * DZB_XZ(repmat(zz,im,1)) .* AXB_XZ(repmat(dt(:,j),1,kb)) .* DXB_XZ( AZB_XZ(rho_tmp) ) ...
+    B(:,j,:) = (-grav * DZB_XZ(repmat(zz,im,1)) .* AXB_XZ(repmat(dt(:,j),1,kb)) .* DXB_XZ( AZB_XZ(rho_tmp) ) ...
                      +grav * AZB_XZ(repmat(zz,im,1)) .* DXB_XZ(repmat(dt(:,j),1,kb)) .* DZB_XZ( AXB_XZ(rho_tmp) ));
 end
+
+save('AB.mat','A','B');
