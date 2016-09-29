233fd945b454a4ea06810df88fabc300c685e217
chenyue15 <katherian@163.com>
2016-09-29 14:24:49 +0800

    The 2nd version

--- new_vertvl.m
+++ new_vertvl.m
@@ -6,16 +6,16 @@ function [xflux,yflux,...
 % * FUN%TION    :  %alculates vertical velocity.                       *
 % *                                                                    *
 % **********************************************************************
-
 xflux = zeros(im,jm,kb);
 yflux = zeros(im,jm,kb);
-      for k=1:kbm1
-          xflux(:,:,k) = AXB1_XY(dy) .* AXB1_XY(dt) .* u(:,:,k);
-      end
-%
-      for k=1:kbm1
-            yflux(:,:,k)=AYB1_XY(dx) .* AYB1_XY(dt) .* v(:,:,k);
-      end
+temp=zeros(im,jm,kb);
+for k=1:kbm1
+    xflux(:,:,k) = AXB1_XY(dy) .* AXB1_XY(dt) .* u(:,:,k);
+    yflux(:,:,k)=AYB1_XY(dx) .* AYB1_XY(dt) .* v(:,:,k); 
+    %etf-etb在边界处不能有值
+    temp(:,:,k)=dz(k).* ( ( DXF2_XY(xflux(:,:,k))+DYF2_XY(yflux(:,:,k)) )...
+                ./(dx.*dy)+OP_L_XY*(etf-etb)/dti2 );
+end
 %
 %     NOTE that, if one wishes to include freshwater flux, the
 %     surface velocity should be set to vflux(i,j). See also
@@ -24,17 +24,10 @@ yflux = zeros(im,jm,kb);
 %
 w = zeros(im,jm,kb);
 w(:,:,1)=0.5*(vfluxb+vfluxf);
-
-%etf-etb在边界处不能有值
-      for k=1:kbm1
-            w(:,:,k+1)=w(:,:,k) + dz(k) ...
-                .* ( ( DXF2_XY(xflux(:,:,k))+DYF2_XY(yflux(:,:,k)) )...
-                ./(dx.*dy)+OP_L_XY*(etf-etb)/dti2 );
-      end
-%
-
-return
+for j=2:jmm1            
+    w(:,j,:)=OP_L_XZ*repmat(w(:,j,1),1,kb)+SUMZ2( permute(temp(:,j,:),[1,3,2]) );
 end
+return
 
 
 
